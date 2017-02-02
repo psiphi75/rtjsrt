87839e317c1992013eeb7d136abacdbf2369f063
Simon Werner <simonwerner@gmail.com>
2017-02-02 15:25:17 +1300

    Changed license.

--- Physics.js
+++ Physics.js
@@ -1,5 +1,34 @@
+/*********************************************************************
+ *                                                                   *
+ *   Copyright 2016 Simon M. Werner                                  *
+ *                                                                   *
+ *   Licensed to the Apache Software Foundation (ASF) under one      *
+ *   or more contributor license agreements.  See the NOTICE file    *
+ *   distributed with this work for additional information           *
+ *   regarding copyright ownership.  The ASF licenses this file      *
+ *   to you under the Apache License, Version 2.0 (the               *
+ *   "License"); you may not use this file except in compliance      *
+ *   with the License.  You may obtain a copy of the License at      *
+ *                                                                   *
+ *      http://www.apache.org/licenses/LICENSE-2.0                   *
+ *                                                                   *
+ *   Unless required by applicable law or agreed to in writing,      *
+ *   software distributed under the License is distributed on an     *
+ *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY          *
+ *   KIND, either express or implied.  See the License for the       *
+ *   specific language governing permissions and limitations         *
+ *   under the License.                                              *
+ *                                                                   *
+ *********************************************************************/
+
 'use strict';
 
+/**
+ * A **very** simple physics implimentation.
+ * @param ground_v
+ * @param options
+ * @constructor
+ */
 function Physics(ground_v, options) {
     this.obj_list = [];
     this.gravity = vector.make(0.0, -0.00981, 0.0);
@@ -37,4 +66,4 @@ Physics.prototype.apply_forces = function () {
     }
 };
 
-
+module.exports = Physics;
